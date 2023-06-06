c71e4e8eb45acd46cbfe49c15eff49e4e49a23ac
Rizky R <700017551@student.curtin.edu.my>
2023-06-06 22:30:28 +0700

    Fix gh-page config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/<repo>/",
+  base: "/calculator/",
   plugins: [react()],
   test: {
 		globals: true,
